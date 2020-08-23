a47bfe9535d05c15187d594f50baf464c0ce829b
Max Perraudin <max.perraudin@gmail.com>
2020-08-23 11:42:27 +0200

    Less ugly solution

--- Weekly_13_Encoding/solution.py
+++ Weekly_13_Encoding/solution.py
@@ -1,14 +1,8 @@
 def solution(text):
     word_list = text.split()
     word_count_dict = {word: word_list.count(word) for word in word_list}
-    word_sorted = {k: v for k, v in sorted(word_count_dict.items(), key=lambda x: x[1], reverse=True)}
-    words = list(word_sorted.keys())
-    output = ""
-    for word in word_list:
-        output += str(words.index(word) + 1) + " "
-    return output.rstrip()
-
-# Ugly AF but it works
+    word_sorted = [k for k, v in sorted(word_count_dict.items(), key=lambda x: x[1], reverse=True)]
+    return " ".join(str(word_sorted.index(word) + 1) for word in word_list)
 
 
 print(solution('hi hi bye hi cya bye'))
